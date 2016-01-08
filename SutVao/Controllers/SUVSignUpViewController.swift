96a48fb963060b5faabee6a9c46f48dba4f087fc
Tan (Tharin) T. NGUYEN <tan.nguyen@enclave.vn>
2016-01-08 17:04:15 +0700

    - Creating UI

---
+++ SutVao/Controllers/SUVSignUpViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SUVSignUpViewController.swift
+//  SutVao
+//
+//  Created by Tan (Tharin) T. NGUYEN on 1/8/16.
+//  Copyright © 2016 MeoSoft. All rights reserved.
+//
+
+import UIKit
+
+class SUVSignUpViewController: UIViewController {
+
+    
+    
+}
